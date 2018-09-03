8a8c3185ce401fdad9df60405a79476a7f69c590
F5XS <f5xs.0000a@gmail.com>
2018-09-04 05:32:15 +0800

    Compiled but runtime errors. Attempting to downgrade to stable futures

--- src/actor.rs
+++ src/actor.rs
@@ -4,6 +4,7 @@ use futures::{
     task::Context as FutContext,
 };
 use futures::executor::Executor;
+use futures_util::FutureExt;
 
 use address::Addr;
 use context::Context;
@@ -28,32 +29,22 @@ pub trait Actor: Sized + 'static + Send {
         // create the context and the first address
         let (mut ctx, addr) = Context::new(self, builder, pool.clone());
 
-        // create a closure wrapped in `ok()` future that will call the function
-        // to poll into ThreadPool.
-        //
-        // the child thread will only be created so the function blocks on
-        // itself but the actual thread that will perform the logic would be
-        // the threadpool
-        let other_thread = lazy(move |_fut_ctx: &mut FutContext| {
-            {
-                // temporarily split the context
-                let (immut_half, mut_half) = ctx.halves_mut();
+        {
+            // temporarily split the context
+            let (immut_half, mut_half) = ctx.halves_mut();
 
-                // call the `on_start()` method of the actor
-                mut_half.actor_mut().on_start(immut_half);
-            }
+            // call the `on_start()` method of the actor
+            mut_half.actor_mut().on_start(immut_half);
+        }
 
-            // put the context in an infinite loop of waiting
-            let loop_future = ctx.fuse().for_each(|_| ok(()));
-
-            // the `on_stop()` isn't called here, it's called on the destructor
-            // of the context.
-
-            loop_future
-        });
-
-        // put the `other_thread` into a waiting loop
-        pool.spawn(Box::new(other_thread));
+        // put the context into a waiting loop
+        pool.spawn(
+            Box::new(
+                ctx.fuse()
+                    .for_each(|_| ok(()))
+                    .map(|_| ())
+            )
+        );
 
         addr
     }
