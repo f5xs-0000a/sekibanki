82f0bd443c24cf03e050c3ab2e51eed7639e0e4e
F5XS <f5xs.0000a@gmail.com>
2018-09-05 09:38:07 +0800

    Added a WeakAddr

--- src/address.rs
+++ src/address.rs
@@ -29,6 +29,13 @@ where
     tx: Sender<Envelope<A>>,
 }
 
+#[derive(Clone)]
+pub struct WeakAddr<A>
+where
+    A: Actor, {
+    tx: Sender<Envelope<A>>,
+}
+
 /// When this object is dropped, it sends a message to the actor to kill itself.
 ///
 /// This is meant to be wrapped in an `Arc` and a clone is held by an address.
@@ -68,19 +75,6 @@ where
         // create the response channel
         let (rtx, rrx) = oneshot();
 
-        // create the closure
-        // let closure = move |actor: &mut A, ctx: &ContextImmutHalf<A>| {
-        // perform the operation and retrieve the response
-        // let response = msg.handle(actor, ctx);
-        //
-        // send the response
-        // rtx.send(response);
-        // };
-        //
-        // send the message closure
-        // self.tx.unbounded_send(Box::new(closure));
-        //
-
         let envelope = Envelope::new(msg, rtx);
         self.tx.unbounded_send(envelope).expect(
             "Message sending unexpectedly failed. Perhaps the receiver \
@@ -89,6 +83,12 @@ where
 
         // return the response channel
         ResponseFuture::with_receiver(rrx)
+    }
+
+    pub fn weak(&self) -> WeakAddr<A> {
+        WeakAddr {
+            tx: self.tx.clone(),
+        }
     }
 }
 
@@ -108,5 +108,30 @@ impl Drop for ActorSelfDestructor {
     fn drop(&mut self) {
         // don't care if it fails
         self.tx.take().unwrap().send(());
+    }
+}
+
+impl<A> WeakAddr<A>
+where
+    A: Actor,
+{
+    fn send<M>(
+        &mut self,
+        msg: M,
+    ) -> ResponseFuture<A, M>
+    where
+        M: Message + 'static,
+        A: Handles<M>, {
+        // create the response channel
+        let (rtx, rrx) = oneshot();
+
+        let envelope = Envelope::new(msg, rtx);
+        self.tx.unbounded_send(envelope).expect(
+            "Message sending unexpectedly failed. Perhaps the receiver \
+             address was also unexpectedly dropped?",
+        );
+
+        // return the response channel
+        ResponseFuture::with_receiver(rrx)
     }
 }
